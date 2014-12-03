9b8418b1514453becba6141b1d4f05e0edf48836
Felipe Castro <felipecastrosm@gmail.com>
2014-12-03 16:23:03 -0200

    Added constructors for Order and Product types

--- ShoppingCart/Domain/Order.cs
+++ ShoppingCart/Domain/Order.cs
@@ -9,5 +9,21 @@ namespace ShoppingCart.Domain
 		public decimal DiscountValue { get; set; }
 
 		public decimal Total { get; set; }
+
+		public Order()
+		{
+			this.Products = new List<Product>();
+		}
+
+		public Order(List<Product> products)
+		{
+			this.Products = products;
+		}
+
+		public Order(List<Product> products, decimal discountValue, decimal total) : this(products)
+		{
+			this.DiscountValue = discountValue;
+			this.Total = total;
+		}
 	}
 }
--- ShoppingCart/Domain/Product.cs
+++ ShoppingCart/Domain/Product.cs
@@ -2,10 +2,17 @@
 {
 	public class Product
 	{
-		public int Id { get; set; }
+		public readonly int Id;
 
 		public string Name { get; set; }
 
 		public decimal Price { get; set; }
+
+		public Product(int id, string name, decimal price)
+		{
+			this.Id = id;
+			this.Name = name;
+			this.Price = price;
+		}
 	}
 }
