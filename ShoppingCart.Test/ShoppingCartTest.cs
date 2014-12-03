60bf976f4462d64f14511604b594ce012a6963ad
Felipe Castro <felipecastrosm@gmail.com>
2014-12-03 16:25:40 -0200

    Created ShoppingCart tests

--- ShoppingCart.Test/ShoppingCartTest.cs
+++ ShoppingCart.Test/ShoppingCartTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
 using ShoppingCart.Domain;
 
@@ -19,8 +15,8 @@ namespace ShoppingCart.Test
 				//Arrange
 				var products = new List<Product>
 				{
-					new Product {Price = 13.75M},
-					new Product {Price = 14.01M}
+					new Product(123, "Fake Product 1", 13.75M),
+					new Product(456, "Fake Product 2", 14.01M)
 				};
 
 				var shoppingCart = new Domain.ShoppingCart(products);
@@ -30,6 +26,96 @@ namespace ShoppingCart.Test
 
 				//Assert
 				Assert.That(totalPrice, Is.EqualTo(27.76M));
+			}
+		}
+
+		public class GetDiscountValue
+		{
+			[Test]
+			public void GivenProductSetAndFixedDiscountGetDiscountValue()
+			{
+				//Arrange
+				var products = new List<Product>
+				{
+					new Product(123, "Fake Product 1", 15M),
+					new Product(456, "Fake Product 2", 35M)
+				};
+
+				var discount = new Discount("ABC", "fixed", 10M);
+
+				var shoppingCart = new Domain.ShoppingCart(products) { Discount = discount };
+
+				//Act
+				var discountValue = shoppingCart.GetDiscountValue();
+
+				//Assert
+				Assert.That(discountValue, Is.EqualTo(10M));
+			}
+
+			[Test]
+			public void GivenProductSetAndPercentageDiscountGetDiscountValue()
+			{
+				//Arrange
+				var products = new List<Product>
+				{
+					new Product(123, "Fake Product 1", 15M),
+					new Product(456, "Fake Product 2", 35M)
+				};
+
+				var discount = new Discount("ABC", "percentage", 10M);
+
+				var shoppingCart = new Domain.ShoppingCart(products) { Discount = discount };
+
+				//Act
+				var discountValue = shoppingCart.GetDiscountValue();
+
+				//Assert
+				Assert.That(discountValue, Is.EqualTo(5M));
+			}
+
+			[Test]
+			public void GivenProductSetAndNullDiscountGetDiscountValue()
+			{
+				//Arrange
+				var products = new List<Product>
+				{
+					new Product(123, "Fake Product 1", 15M),
+					new Product(456, "Fake Product 2", 35M)
+				};
+
+				var shoppingCart = new Domain.ShoppingCart(products);
+
+				//Act
+				var discountValue = shoppingCart.GetDiscountValue();
+
+				//Assert
+				Assert.That(discountValue, Is.EqualTo(0));
+			}
+		}
+
+		public class GetOrder
+		{
+			[Test]
+			public void GivenProductSetAndDiscountGetOrder()
+			{
+				//Arrange
+				var products = new List<Product>
+				{
+					new Product(123, "Fake Product 1", 15M),
+					new Product(456, "Fake Product 2", 35M)
+				};
+
+				var discount = new Discount("ABC", "percentage", 10M);
+
+				var shoppingCart = new Domain.ShoppingCart(products) { Discount = discount };
+
+				//Act
+				var order = shoppingCart.GetOrder();
+
+				//Assert
+				Assert.That(order.Products, Is.EquivalentTo(products));
+				Assert.That(order.DiscountValue, Is.EqualTo(5M));
+				Assert.That(order.Total, Is.EqualTo(45M));
 			}
 		}
     }
