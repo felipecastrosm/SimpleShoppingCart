e4ea9fe19445f2eaa79d4245739c51d7ea2c9212
Felipe Castro <felipecastrosm@gmail.com>
2014-12-03 23:08:50 -0200

    Created tests for Discount

---
+++ ShoppingCart.Test/DiscountTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using ShoppingCart.Domain;
+using ShoppingCart.Domain.Enum;
+using ShoppingCart.Exceptions;
+
+namespace ShoppingCart.Test
+{
+	[TestFixture]
+	public class DiscountTest
+	{
+		public class GetDiscountTypeEnumValue
+		{
+			[Test]
+			public void GivenValidTypeStringShouldReturnEnumType()
+			{
+				//Arrange
+				const string validType = "fixed";
+				var discount = new Discount();
+
+				//Act
+				var enumType = discount.GetDiscountTypeEnumValue(validType);
+
+				//Assert
+				Assert.That(enumType, Is.TypeOf<DiscountType>());
+				Assert.That(enumType, Is.EqualTo(DiscountType.Fixed));
+			}
+
+			[Test]
+			public void GivenInvalidTypeStringShouldThrowUnknownDiscountTypeException()
+			{
+				//Arrange
+				const string invalidType = "invalidType";
+				var discount = new Discount();
+
+				//Act
+
+				//Assert
+				Assert.Throws<UnknownDiscountTypeException>(() => discount.GetDiscountTypeEnumValue(invalidType));
+			}
+		}
+
+		public class GetDiscountTypeStringValue
+		{
+			[Test]
+			public void GivenValidEnumTypeShouldReturnString()
+			{
+				//Arrange
+				const DiscountType validEnumType = DiscountType.Percentage;
+				var discount = new Discount();
+
+				//Act
+				var returnString = discount.GetDiscountTypeStringValue(validEnumType);
+
+				//Assert
+				Assert.That(returnString, Is.EqualTo("percentage"));
+			}
+		}
+	}
+}
