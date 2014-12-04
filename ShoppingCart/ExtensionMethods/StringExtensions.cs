de052a701f717811aafd0be8ede20719d302fce1
Felipe Castro <felipecastrosm@gmail.com>
2014-12-03 22:37:47 -0200

    Created StringExtensions.Truncate to help handling long product names

---
+++ ShoppingCart/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace ShoppingCart.ExtensionMethods
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Truncates a string to a fixed length adding an optional suffix
+		/// </summary>
+		/// <param name="originalString">The string to be truncated</param>
+		/// <param name="maxSize">The length it should be truncated to</param>
+		/// <param name="suffix">The optional suffix (Defaults to "...")</param>
+		/// <returns></returns>
+		public static string Truncate(this string originalString, int maxSize, string suffix = "...")
+		{
+			originalString = originalString.Trim();
+
+			if (originalString.Length <= maxSize)
+			{
+				return originalString;
+			}
+
+			var usableLength = maxSize - suffix.Length;
+
+			var finalString = originalString.Substring(0, usableLength) + suffix;
+
+			return finalString;
+		}
+	}
+}
